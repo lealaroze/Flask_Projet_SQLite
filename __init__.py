46c2249fdfed97673b84cdbead407d9852921bef
lealaroze <72609753+lealaroze@users.noreply.github.com>
2024-07-18 14:42:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ def est_authentifie():
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #Comm3
+    return render_template('hello.html') #Comm4
 
 @app.route('/lecture')
 def lecture():
